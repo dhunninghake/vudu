e8958cc4a5b39251f4b274f07e23023cea9180f1
Daniel Hunninghake <dhunn1587@gmail.com>
2018-03-03 10:39:58 -0500

    Enhancements to rollup setup

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import gzip from 'rollup-plugin-gzip';
 
 export default {
   entry: 'src/vudu.js',
@@ -16,12 +18,16 @@ export default {
       jsnext: true,
       main: true,
       browser: true,
-      extensions: [ '.js', '.json' ],
+      extensions: ['.js', '.json'],
       preferBuiltins: false
     }),
     commonjs({
       include: 'node_modules/**'
     }),
+    filesize({
+      showGzippedSize: true
+    }),
+    gzip()
   ],
   dest: 'dist/vudu.js'
 };
