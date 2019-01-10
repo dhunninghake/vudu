dd157f1f3ea9e7dcda259032db979423d8bb6671
Daniel Hunninghake <dhunn1587@gmail.com>
2019-01-09 19:29:52 -0500

    Remove composes object

--- src/vudu.js
+++ src/vudu.js
@@ -1,7 +1,6 @@
 import deepEqual from 'deep-equal';
 import uniqueId from 'lodash.uniqueid';
 import { createSheet } from './utils';
-import { composes, config } from './composes';
 import { attachRule, addRule } from './attach';
 import { formatRule } from './format';
 
@@ -9,19 +8,9 @@ let cache = [];
 
 let vuduSheet = createSheet('vSheet');
 
-/**
- * buildRuleset:
- * 1) Generates unique vudu classes
- * 2) Adds formatted rules to the sheet
- * 3) Returns an object of vudu classes
- *
- * @param {Object} group
- * @param {Object} sheet
- * @param {Object} [options={}]
- * @returns {Object}
- */
-const buildRuleset = (group, sheet, options={}) => {
-  const suffix = typeof options.suffix === 'function' ? options.suffix() : options.suffix;
+const buildRuleset = (group, sheet, options = {}) => {
+  const suffix =
+    typeof options.suffix === 'function' ? options.suffix() : options.suffix;
   const rules = Object.keys(group).map(classname => {
     return {
       classname,
@@ -30,21 +19,16 @@ const buildRuleset = (group, sheet, options={}) => {
     };
   });
   rules.forEach(r => addRule(formatRule(r.styles), r.vuduClass, sheet, true));
-  return rules.reduce((a, b) => ({
-    ...a,
-    [b.classname]: b.vuduClass,
-  }), {});
+  return rules.reduce(
+    (a, b) => ({
+      ...a,
+      [b.classname]: b.vuduClass,
+    }),
+    {}
+  );
 };
 
-/**
- * V kicks off adding a new rule.
- *
- * @param {Object} el
- * @param {Object} [customSheet]
- * @param {Object} [options={}]
- * @returns {Object}
- */
-const v = (el, customSheet, options={}) => {
+const v = (el, customSheet, options = {}) => {
   const cachedItem = cache.find(item => deepEqual(item.element, el));
   if (cachedItem) {
     return cachedItem.classes;
@@ -60,9 +44,6 @@ const v = (el, customSheet, options={}) => {
   return classes;
 };
 
-/**
- * PUBLIC METHODS
- */
 const addFontFace = (font, customSheet) => {
   const sheet = customSheet || vuduSheet;
   const dec = formatRule(font)
@@ -81,19 +62,11 @@ const logOutput = () => {
   );
 };
 
-/**
- * Curries v to make configuring v easier
- *
- * @param {Object} options
- * @returns {Function}
- */
-const options = (options) => (el, customSheet) => v(el, customSheet, options)
+const options = options => (el, customSheet) => v(el, customSheet, options);
 
-v.addFontFace = addFontFace;
-v.logOutput = logOutput;
-v.composes = composes;
-v.config = config;
-v.options = options;
-v.v = v;
-
-export default v;
+export default {
+  addFontFace,
+  logOutput,
+  options,
+  v,
+};
